e8e0e1e9aa44fe47ca7dd7c41422687d87f8a1e8
AleDegio <50446554+AleDegio@users.noreply.github.com>
2019-10-16 11:01:06 +0200

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,22 @@
+/*
+Alessia De Giovannini
+es malloc
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  int n;
+  int i;
+
+  printf("quanti numeri vuoi? ");
+  scanf("%d", &n);
+
+  int *vet=(int*)malloc(n*sizeof(int));
+  printf("\n");
+  for(i=0; i<n; i++){
+      printf("Inserisci il numero nella cella [%d]: ", i);
+      scanf("%d", &vet+i);
+   }
+ }
